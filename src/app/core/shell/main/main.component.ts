a4ae97e50901f3b6c5576dd92ab0a164043c85db
kzurro <kzurro@hotmail.com>
2021-03-30 19:39:34 +0200

    Base de una Aplicación Angular

---
+++ src/app/core/shell/main/main.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  title: string = environment.appName;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
