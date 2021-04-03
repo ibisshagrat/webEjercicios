b3a7f0cc4f2907969ca614424cb32bcec230c10e
kzurro <kzurro@hotmail.com>
2021-04-03 19:25:13 +0200

    Estructuras parte 2 de 3

--- src/app/contacts/contacts/contacts.component.ts
+++ src/app/contacts/contacts/contacts.component.ts
@@ -13,7 +13,20 @@ export class ContactsComponent implements OnInit {
   public numContacts = 0;
   public counterClass = 'tag secondary';
   public formHidden = false;
-  public contact = { name: '', isVIP: false, gender: '' };
+  public workStatuses = [
+    { id: 0, description: 'unknow' },
+    { id: 1, description: 'student' },
+    { id: 2, description: 'unemployed' },
+    { id: 3, description: 'employed' }
+  ];
+  public contact = {
+    name: '',
+    isVIP: false,
+    gender: '',
+    workStatus: '0',
+    company: '',
+    education: ''
+  };
   constructor() { }
 
   ngOnInit(): void {
