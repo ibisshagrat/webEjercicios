8eb75aab72d447420d4d90633857e270e90b3155
kzurro <kzurro@hotmail.com>
2021-04-03 22:10:02 +0200

    Estructuras 3-3

--- src/app/contacts/contacts/contacts.component.ts
+++ src/app/contacts/contacts/contacts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../models/contact';
+import { Option } from '../models/option';
 
 @Component({
   selector: 'app-contacts',
@@ -13,23 +15,38 @@ export class ContactsComponent implements OnInit {
   public numContacts = 0;
   public counterClass = 'tag secondary';
   public formHidden = false;
-  public workStatuses = [
-    { id: 0, description: 'unknow' },
-    { id: 1, description: 'student' },
-    { id: 2, description: 'unemployed' },
-    { id: 3, description: 'employed' }
-  ];
-  public contact = {
-    name: '',
-    isVIP: false,
-    gender: '',
-    workStatus: '0',
-    company: '',
-    education: ''
-  };
+  public workStatuses: Option[];
+  public contact: Contact;
+  public contacts: Contact[];
   constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.workStatuses = [
+      { id: 0, description: 'unknow' },
+      { id: 1, description: 'student' },
+      { id: 2, description: 'unemployed' },
+      { id: 3, description: 'employed' }
+    ];
+    this.contact = {
+      name: '',
+      isVIP: false,
+      gender: '',
+      workStatus: 0,
+      company: '',
+      education: ''
+    };
+    this.contacts = [];
   }
+
+
+  public saveContact(): void {
+  this.contacts.push({ ...this.contact });
+  this.numContacts = this.contacts.length;
+  console.log(this.contacts);
+}
+
+  public deleteContact(contact: Contact): void {
+  this.contacts = this.contacts.filter(c => c.name !== contact.name);
+  this.numContacts = this.contacts.length;
+}
 
 }
