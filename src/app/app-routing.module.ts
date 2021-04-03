2b86c327c69c558eb6b8987dfd11120bc9cb574a
kzurro <kzurro@hotmail.com>
2021-04-03 18:42:17 +0200

    Formularios primera parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   },
   {
+    path: 'contacts',
+    loadChildren: () => import ('./contacts/contacts.module').then (m => m.ContactsModule)
+    },
+  {
     path: 'heroes',
     component: HeroesComponent
   },
